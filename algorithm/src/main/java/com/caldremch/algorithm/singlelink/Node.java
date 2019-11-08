c018201c0fdc32809026fe038fdd4418267961ec
Caldremch <caldremch@163.com>
2019-11-08 17:49:12 +0800

    add files

---
+++ algorithm/src/main/java/com/caldremch/algorithm/singlelink/Node.java
@@ -0,0 +1,9 @@
+package com.caldremch.algorithm.singlelink;
+
+/**
+ * Created by Caldremch on 2019/11/8.
+ */
+public class Node {
+	private int data;
+	private Node next;
+}
---
+++ algorithm/src/main/java/com/caldremch/algorithm/singlelink/SingleLinkList.java
@@ -0,0 +1,27 @@
+package com.caldremch.algorithm.singlelink;
+
+/**
+ * Created by Caldremch on 2019/11/8.
+ * 增删改查
+ */
+public class SingleLinkList {
+
+	public void insert(Node node) {
+
+	}
+
+	public void delete(int i) {
+
+	}
+
+	public Node get(int i) {
+		return null;
+	}
+
+	public void add(Node node) {
+
+	}
+	
+}
+
+
