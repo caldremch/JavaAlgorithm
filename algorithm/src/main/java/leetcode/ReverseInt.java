f2eeb3aa7683847405341a6a4b0f86ba9e255dba
Caldremch <caldremch@163.com>
2020-10-09 09:30:56 +0800

    ReverseInt

--- algorithm/src/main/java/leetcode/ReverseInt.java
+++ algorithm/src/main/java/leetcode/ReverseInt.java
@@ -6,20 +6,28 @@ public class ReverseInt extends BaseRunner {
 
     @Override
     protected void onRun() {
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(exec(-214748));
+        System.out.println(exec(463847412));
     }
 
-    private int exec(int x){
-        long res = 0;
-        while (x!=0){
-            int temp = x%10;
-            res = res*10+temp;
-            if (res>Integer.MAX_VALUE || res<Integer.MIN_VALUE){
+    /**
+     * 2147483647
+     * @param x
+     * @return
+     */
+    private int exec(int x) {
+        int res = 0;
+        while (x != 0) {
+            int temp = x % 10;
+            x = x / 10;
+            //Integer.MAX_VALUE / 10 = 7
+            if (res > Integer.MAX_VALUE / 10 || res == Integer.MAX_VALUE / 10 && temp > 7) {
                 return 0;
             }
-            x  = x/10;
+            if (res < Integer.MIN_VALUE / 10 || res == Integer.MIN_VALUE / 10 && temp < -8) {
+                return 0;
+            }
+            res = res * 10 + temp;
         }
-        return (int)res;
+        return res;
     }
 }
